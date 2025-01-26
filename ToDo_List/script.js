5352b21f422307eee5bfce954dc592ad864c6a35
mubashir12-a <ma2625645@gmail.com>
2025-01-26 16:09:17 +0530

    version V.4

--- ToDo_List/script.js
+++ ToDo_List/script.js
@@ -1,92 +1,144 @@
 'use strict'
 
-let AddBtn = document.getElementById("AddList");
-let InputText = document.getElementById("InputTag");
-let Limit, trigger, IDFind = 1;
-let CountUpt = document.getElementById("countAlpha");
-let AlertMsg = document.getElementById("ErrMsg");
-let AlertImg = document.getElementById("AlertImg");
-let PlaceHolder = document.getElementById("InputPlaceHolder");
+const AddBtn = document.getElementById("AddList");
+const InputText = document.getElementById("InputTag");
+const DelBtn = document.getElementById("List");
+const CountUpt = document.getElementById("countAlpha");
+const AlertMsg = document.getElementById("ErrMsg");
+const AlertImg = document.getElementById("AlertImg");
+const PlaceHolder = document.getElementById("InputPlaceHolder");
+let contBox = document.getElementById("List");
+let Initial_Limit, CurrentCount;
+let BtnDel;
+let Cont;
+let InputBox;
+let InputString, Alphabets;
+let Len;
+let labelList;
+
+let DarkLight = document.getElementById("DarK-Light");
+let InDrk = document.getElementById("inBtn");
+let DrkSwitch = true;
 
 
-InputText.addEventListener('input', () => {
-    Limit = 100;
-    let velChk = InputText.value;
-    let Len = velChk.length;
-    Limit = Limit - Len;
-    
+DarkLight.addEventListener('click', () => {
+  if(DrkSwitch){
+    DarkLight.style.justifyContent = "flex-end";
+    DarkLight.style.background = "#222222";
+    InDrk.style.background = "white";
 
-    if((velChk.trim(" ")).length > 100){
-      AlertImg.style.opacity = "1";
-      AlertMsg.textContent = "Limit Reached";
-      InputText.value = InputText.value.slice(0,100);
-    }else{
-      if(Limit >= 0 && Limit <= 100){
-        CountUpt.textContent = Limit;
-      }
+    document.documentElement.style.setProperty("--bg", "white");
+    document.documentElement.style.setProperty("--bg_2", "black");
+    document.documentElement.style.setProperty("--TextWhite", "black");
+    document.documentElement.style.setProperty("--TextListBlk", "rgb(75, 71, 71)");
+    document.documentElement.style.setProperty("--placeHolbg", "white");
+    document.documentElement.style.setProperty("--listbg", "#222222");
+    DrkSwitch = false;
+  }else {
+    DarkLight.style.justifyContent = "flex-start";
+    DarkLight.style.background = "#fff";
+    InDrk.style.background = "black";
 
-      AlertImg.style.opacity = "0";
-      AlertMsg.textContent = "";
-      InputText.style.height = InputText.scrollHeight + "px";
-    }
+    document.documentElement.style.setProperty("--bg", "black");
+    document.documentElement.style.setProperty("--bg_2", "white");
+    document.documentElement.style.setProperty("--TextWhite", "white");
+    document.documentElement.style.setProperty("--TextlistBlk", "#rgba(255, 255, 255, 0.478)");
+    document.documentElement.style.setProperty("--placeHolbg", "black");
+    document.documentElement.style.setProperty("--listbg", "#ffffff3d");
+    DrkSwitch = true;
+  }
 })
 
 
-function AddNewEle(){
+InputText.addEventListener('input', () => {
+  Initial_Limit = 100;
+  InputString = InputText.value;
+  Alphabets = InputString.replace(/\s+/g, "");
+
+  //console.log("Alphabets = ", Alphabets);
+  
+  Len = Alphabets.length;
+  //console.log("Length = ", Len);
+  CurrentCount = Initial_Limit - Len;
+  //console.log("Current = ", CurrentCount);
+
+  console.log("\n\n");
+  console.log(" Actual slice lim = ", InputText.value.length - (Len - Initial_Limit));
+  console.log(" Actual slice Input len = ", InputText.value.length);
+  console.log(" Actual slice Initial lim = ", Initial_Limit);
+  console.log(" Actual slice len = ", Len);
+  console.log(" Actual slice len - initial = ", (Len - Initial_Limit));
+  console.log(InputText.value.slice(0, InputText.value.length - (Len - Initial_Limit)));
+
+  if (CurrentCount < 0) {
+    AlertImg.style.opacity = "1";
+    AlertMsg.textContent = "Letter Limit Reached";
+    InputText.value = InputText.value.slice(0, InputText.value.length - (Len - Initial_Limit));
+  } else {
+    if (CurrentCount >= 0 && CurrentCount <= Initial_Limit) {
+      CountUpt.textContent = CurrentCount;
+    }
+
+    AlertImg.style.opacity = "0";
+    AlertMsg.textContent = "";
+    InputText.style.height = InputText.scrollHeight + "px";
+  }
+})
+
+
+function AddNewEle() {
   CountUpt.textContent = 100;
 
 
-  let labelList = document.createElement("label");
-    labelList.textContent = InputText.value;
+  labelList = document.createElement("label");
+  labelList.textContent = InputText.value;
 
-    if((InputText.value.trim()) !== ""){
-        let Cont = document.createElement("div");
-        let InputBox = document.createElement("input");
-        InputBox.type = "checkbox";
-        let BtnDel = document.createElement("button");
-        BtnDel.classList.add("Del-btn");
+  if ((InputText.value.trim()) !== "") {
+    Cont = document.createElement("div");
+    InputBox = document.createElement("input");
+    InputBox.type = "checkbox";
+    BtnDel = document.createElement("button");
+    BtnDel.classList.add("Del-btn");
 
-        Cont.appendChild(InputBox);
-        Cont.appendChild(labelList);
-        Cont.appendChild(BtnDel);
+    Cont.appendChild(InputBox);
+    Cont.appendChild(labelList);
+    Cont.appendChild(BtnDel);
 
-        let contBox = document.getElementById("List");
 
-        contBox.appendChild(Cont);
-        InputText.value = "";
-    }else{
-      PlaceHolder.style.color = "red";
-        PlaceHolder.innerText = "Please Write First";
-        PlaceHolder.animate([
-            {transform : "translateX(0%)"},
-            {transform : "translateX(5%)"},
-            {transform : "translateX(0%)"},
-            {transform : "translateX(10%)"},
-            {transform : "translateX(0%)"}
-        ],{
-            duration: 200,
-            iterations: 2,
-            easing: "ease-in-out"
-        })
+    contBox.appendChild(Cont);
+    InputText.value = "";
+  } else {
+    PlaceHolder.style.color = "red";
+    PlaceHolder.innerText = "Please Write First";
+    PlaceHolder.animate([
+      { transform: "translateX(0%)" },
+      { transform: "translateX(5%)" },
+      { transform: "translateX(0%)" },
+      { transform: "translateX(10%)" },
+      { transform: "translateX(0%)" }
+    ], {
+      duration: 200,
+      iterations: 2,
+      easing: "ease-in-out"
+    })
 
-        InputText.focus();
-    }
+    InputText.focus();
+  }
 }
 
 AddBtn.addEventListener("click", () => {
-    AddNewEle();
+  AddNewEle();
 });
 
 InputText.addEventListener('keydown', (e) => {
-  if(e.key === "Enter"){
+  if (e.key === "Enter") {
     AddNewEle();
   }
 })
 
-let DelBtn = document.getElementById("List");
 
 DelBtn.addEventListener('click', (e) => {
-  if(e.target.classList.contains("Del-btn")){
+  if (e.target.classList.contains("Del-btn")) {
     e.target.parentNode.remove();
   }
 })
